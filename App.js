b1badc6f095e56e70e091a60610bd68321910048
cristixndr3s <Cristiandres17@gmail.com>
2021-05-12 23:26:01 -0500

    jjjj

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
+
+const gray = "#91A1BD";
 
 export default function App() {
+  const NeuMorph = ({ children, size, style }) => {
+    return(
+      <View style={styles.topShadow}>
+      <View style={styles.bottomShadow}>
+      <View
+      style={[
+        styles.inner,
+        { width: size || 40, height: size || 40, borderRadius: size / 2 || 40 / 2},
+      style
+    ]}
+      >
+       {children}
+      </View>
+      </View>
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
-    <Text>Intentando construir un portafolio movil </Text>
+    <SafeAreaView style={{ alignSelf: "stretch"}}>
+    <View style = {{ marginHorizontal: 32, marginTop: 32 }}>
+    <View style = {styles.topContainer}>
+    <NeuMorph>
+      <AntDesign name="arrowleft" size={20} color={gray} />
+    </NeuMorph>
+    <View>
+    <Text style = {styles.playing}>Cristixndr3s</Text>
+    </View>
+    <NeuMorph>
+      <Entypo name = "menu" size = {24} color ={gray}/>
+    </NeuMorph>
+    </View>
+    </View>
+    </SafeAreaView>
     </View>
   );
 }
@@ -14,6 +48,36 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#DEE9FD",
     alignItems: "center",
-    justifyContent: "center"
-  }
+    },
+    topContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center"
+    },
+    inner: {
+      backgroundColor: "#DEE9F7",
+      alignItems:"center",
+      justifyContent: "center",
+      borderColor: "#E2ECFD",
+      borderWidth: 1
+    },
+    topShadow: {
+      shadowOffset: {
+        width: -6,
+        height: -6
+      },
+      shadowOpacity: 1,
+      shadowRadius: 6,
+      shadowColor: "#FBFFFF"
+    },
+    bottomShadow: {
+      shadowOffset:{
+        width: 6,
+        height: 6
+      },
+      shadowOpacity: 1,
+      shadowRadius: 6,
+      shadowColor: "#B7C4DD"
+    },
+
 });
